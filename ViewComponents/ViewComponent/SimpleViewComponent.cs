dcd6eacf37bf5e653238c9ecb14ee5c296963768
rangu <rangu@RANGESH>
2016-07-30 21:36:40 +0530

    Add project files.

---
+++ ViewComponents/ViewComponent/SimpleViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.ViewComponent
+{
+    public class SimpleViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
